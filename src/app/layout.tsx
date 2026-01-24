04c0e31da442ca0b4f043a704bc07ce2c0d67e83
bizsetter7 <bizsetter7@gmail.com>
2026-01-25 00:58:17 +0900

    화면비율 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
+      <body className={`${inter.className} overflow-x-hidden`}>
         <Suspense fallback={<div>Loading...</div>}>
           <BrandProvider>
             {children}
