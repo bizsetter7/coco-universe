ac8a126cdc8f5a11ba8a6d7156e3412d43fc1773
bizsetter7 <bizsetter7@gmail.com>
2026-01-25 00:08:59 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { BrandProvider } from "@/components/BrandProvider";
@@ -12,6 +12,13 @@ export const metadata: Metadata = {
   verification: {
     google: 'enzbVhzoI9Bq9YzGqFaLghzkqVlFHwe-DBnnNajWC0Y',
   },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
