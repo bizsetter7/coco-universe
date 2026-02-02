2c0653a765be67652d2602e6c9b54dee5f172b64
bizsetter7 <bizsetter7@gmail.com>
2026-02-03 05:08:16 +0900

    fix: refactor banner rail structure - aside as long container and internal div as sticky

--- src/components/BannerSidebar.tsx
+++ src/components/BannerSidebar.tsx
@@ -93,7 +93,7 @@ export const BannerSidebar = ({ side }: BannerSidebarProps) => {
     return (
         <>
             <div
-                className="relative w-[160px] flex flex-col gap-2 z-[40] self-start"
+                className="sticky top-[120px] w-[160px] flex flex-col gap-2 z-[40] self-start"
             >
                 {/* 내부 모든 요소는 클릭 가능하도록 설정 */}
                 <div className="flex flex-col gap-2">
